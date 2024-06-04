ace5113031ebb7c157b751e7d2c6ca1c2556fe72
Carlos Henrique Alves <carloshadp@gmail.com>
2024-06-04 20:23:33 -0300

    Melhorando e ajustando o Login e Registro

--- JS/src/registro.js
+++ JS/src/registro.js
@@ -1,41 +1,29 @@
-// LOGIN
-
-const user = document.querySelector("#username");
-const senha = document.querySelector("#password");
-const entrar = document.querySelector("#entrar");
-
-entrar.addEventListener("click", function(){
-    if(!nomes.includes(user.value)){
-        alert("Este user nao tem conta");
-    }else if(nomes.includes(user.value) && !senhas.includes(senha.value)){
-        alert("Senha icorreta")
-    }
-    if(nomes.includes(user.value) && senhas.includes(senha.value)){
-        window.location.href = "/Landing page - GS/src/home/index.html"
-    }
-})
-
-//REGISTRAR
 const nomes = JSON.parse(localStorage.getItem('nomes')) || [];
 const senhas = JSON.parse(localStorage.getItem('senhas')) || [];
 const criarUser = document.querySelector("#criarUsername");
 const criarSenha = document.querySelector("#criarPassword");
 const criarConta = document.querySelector("#criarConta");
 
+
 criarConta.addEventListener("click", function(){
-    
-    if(!nomes.includes(criarUser.value)){
+    if (criarSenha.value === "" || criarUser.value === ""){
+        return;
+    }
+    if(!nomes.includes(criarUser.value) && criarUser.value !== null){
         nomes.push(criarUser.value);
         localStorage.setItem('nomes', JSON.stringify(nomes));
+        alert("Conta criada");
     }else{
         alert("Este user ja tem conta, Volte e faca login")
     }
-    if(!senhas.includes(criarSenha.value)){
+    if(!senhas.includes(criarSenha.value) && criarSenha.value !== null){
         senhas.push(criarSenha.value);
         localStorage.setItem('senhas', JSON.stringify(senhas));
     }
+    
     if(nomes.includes(criarUser) && senhas.includes(criar)){
         alert("Conta criada, pode voltar ao login!")
     }
-
+    criarUser.value = "";
+    criarSenha.value = "";
 })
